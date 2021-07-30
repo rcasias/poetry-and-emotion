f18ec5cbedc0473ebfeea9383d9b3bbd1f648df8
Regina Casias <reginaannvigil@gmail.com>
2021-07-30 11:21:53 -0600

    Facade

---
+++ app/facades/poetry_facade.rb
@@ -0,0 +1,9 @@
+class PoetryFacade
+  def self.author_search(author)
+    data = PoetryApiService.search(author)
+    data.map do |attrs|
+      Author.new(attrs)
+    end
+    # binding.p
+  end
+end
---
+++ spec/facades/poetry_facade_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe 'PoetryFacade' do
+  it 'it should call facade and retrieve seach results' do
+    author_name = "Adam%20Lindsay%20Gordon"
+    result = PoetryFacade.author_search(author_name)
+
+
+    expect(result).to be_an Array
+    first_author = result.first
+    expect(first_author).to be_a(Author)
+    expect(first_author.title).to eq("A Song of Autumn")
+    expect(first_author.author).to eq("Adam Lindsay Gordon")
+    expect(first_author.lines[0]).to eq("‘WHERE shall we go for our garlands glad")
+    expect(first_author.linecount).to eq("16")
+  end
+end
