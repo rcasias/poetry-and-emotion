c521eab8669d5700e71f7f6917b4686b7ea21e8e
Regina Casias <reginaannvigil@gmail.com>
2021-08-02 16:10:32 -0600

    refactored ibm service

--- app/services/ibm_api_service.rb
+++ app/services/ibm_api_service.rb
@@ -1,20 +1,28 @@
 class IbmApiService
 
   def self.analyze(text)
-    response = base_conn.post('/instances/f7a8d152-93c5-4c8f-ad5f-5b5a4fe172fe/v3/tone') do |req|
-      req.body = {'text' => text}.to_json
+    response = base_conn.get('/v3/tone') do |req|
+      # req.body = {'text' => text}.to_json
+      req.params['version']='2017-09-21'
+      req.params['text'] = text
     end
     JSON.parse(response.body, symbolize_names: true)
   end
 
+  # def self.base_conn
+  #   Faraday.new(
+  #     url: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com',
+  #     headers: {'Content-Type' => 'application/json'},
+  #     params: {version: '2017-09-21'}
+  #   ) do |conn|
+  #     conn.basic_auth('apikey', ENV['IBM_API_KEY'])
+  #   end
+  # end
+
   def self.base_conn
-    Faraday.new(
-      url: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com',
-      headers: {'Content-Type' => 'application/json'},
-      params: {version: '2017-09-21'}
-    ) do |conn|
-      conn.basic_auth('apikey', ENV['IBM_API_KEY'])
+    Faraday.new(url: 'https://api.us-south.tone-analyzer.watson.cloud.ibm.com/instances/f7a8d152-93c5-4c8f-ad5f-5b5a4fe172fe') do |connection|
+      connection.basic_auth('apikey', ENV['IBM_API_KEY'] )
     end
   end
-  
+
 end
--- spec/services/ibm_api_service_spec.rb
+++ spec/services/ibm_api_service_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe IbmApiService do
   it "searches our API Service" do
     results = IbmApiService.analyze('im so happy')
     tone = results[:document_tone][:tones][0][:tone_name]
-
+    binding.pry
     expect(tone).to eq("Joy")
   end
 end
